c0756a24387bf27794c1cb77447d914b283424cf
Marcos Serpa <marcosserpa@gmail.com>
2015-11-15 12:28:41 -0200

    Almost finish the job

--- lib/number_prettifier.rb
+++ lib/number_prettifier.rb
@@ -4,25 +4,61 @@ class NumberPrettifier
 
   class << self
 
-    def prettify
-      puts "Hi pseudo-jedi! This is the number prettifier. Enter a number with more than 6 digits and wich is less than or equal to 999999999999999 (999 trillion):"
-      number = gets.chomp
+    def prettify(number = nil)
+      unless not number.nil?
+        begin
+          puts "Hi pseudo-jedi! This is the number prettifier. Enter a number with more than 6 digits and wich is less than or equal to 999999999999999 (999 trillion):"
+          number = gets.chomp
+        end until verify_input(number)
+      end
+
+      truncate_number(number.to_s)
     end
 
 
     private
 
     def verify_input(number)
-      begin
-        raise "You must to provide a number, young padawan!" if number.empty?
-        raise "You must to provide a number with more than 6 digits." if number.size < 7
-        raise "You must to provide a number which is less than or equal to 999.999.999.999.999 (but without the dots, young!)" if number.to_i > 999999999999999
-      rescue RuntimeError => e
-        false
-      end
+      raise "You must provide a number, young padawan!" if number.empty?
+      raise "You must provide a number which is less than or equal to 999.999.999.999.999 (but without the dots, young!)" if number.to_i > 999999999999999
 
       true
     end
+
+    def truncate_number(number)
+      return number if number.size < 7
+
+      if [7, 8, 9].include? number.size
+        "#{ verify_magnitude(number, -7) }M"
+      elsif [10, 11, 12].include? number.size
+        "#{ verify_magnitude(number, -10) }B"
+      else
+        "#{ verify_magnitude(number, -13) }T"
+      end
+    end
+
+    def verify_magnitude(number, position)
+      trunc_i = number.dup.insert(position, '.').to_i
+      trunc_f = number.insert(position, '.').to_f  #(number.insert(position, '.').to_f * 10).truncate / 10
+
+      trunc_i == trunc_f ? trunc_i : trunc_f
+    end
+
+    #def remove_trailing_digits(number)
+    #  result = ''
+    #  previous = ''
+
+    #  number.chars.each do |c|
+    #    if previous == '.'
+    #      result << c
+    #      break
+    #    end
+
+    #    result << previous = c
+    #  end
+
+    #  result
+    #end
 
   end
 
--- spec/number_prettifier_spec.rb
+++ spec/number_prettifier_spec.rb
@@ -6,17 +6,57 @@ describe NumberPrettifier do
   describe '.verify_input' do
     context 'not given a number' do
       it "returns a message" do
-        expect{ NumberPrettifier.send(:verify_input, '') }.to raise_error(RuntimeError, "You must to provide a number, young padawan!")
+        expect{ NumberPrettifier.send(:verify_input, '') }.to raise_error(RuntimeError, "You must provide a number, young padawan!")
       end
     end
 
     context "given a number" do
       it "has less than 7 digits" do
-        expect{ NumberPrettifier.send(:verify_input, '123456') }.to raise_error(RuntimeError, "You must provide a number with more than 6 digits.")
+        expect(NumberPrettifier.send(:verify_input, '123456')).to be(true)
       end
       
       it "is greater than 999.999.999.999.999" do
-        expect{ NumberPrettifier.send(:verify_input, '10000000000000') }.to raise_error(RuntimeError, "You must to provide a number which is less than or equal to 999.999.999.999 (but without the dots, young!)")
+        expect{ NumberPrettifier.send(:verify_input, '1000000000000000') }.to raise_error(RuntimeError, "You must provide a number which is less than or equal to 999.999.999.999.999 (but without the dots, young!)")
+      end
+    end
+
+    context "given a valid number" do
+      it "returns true" do
+        expect(NumberPrettifier.send(:verify_input, '83487893843789')).to be(true)
+      end
+    end
+  end
+
+  describe '.truncate_number' do
+    context "number has 7, 8 or 9 digits" do
+      it "returns the prettified Millions" do
+        expect(NumberPrettifier.send(:truncate_number, '77000000')).to eql("77M")
+      end
+    end
+
+    context "number has 10, 11 or 12 digits" do
+      it "returns the prettified Billions" do
+        expect(NumberPrettifier.send(:truncate_number, '8800000000')).to eql("8.8B")
+      end
+    end
+
+    context "number has 13, 14 or 15 digits" do
+      it "returns the prettified Trillions" do
+        expect(NumberPrettifier.send(:truncate_number, '99990000000000')).to eql("99.9T")
+      end
+    end
+  end
+
+  describe ".verify_magnitude" do
+    context "given a next magnitude of zeros" do
+      it "returns a integer short scale" do
+        expect(NumberPrettifier.send(:verify_magnitude, '99000000', -7)).to eql(99)
+      end
+    end
+
+    context "given a next magnitude of non-zeros" do
+      it "returns a float short scale" do
+        expect(NumberPrettifier.send(:verify_magnitude, '43700000000000')).to eql(43.7)
       end
     end
   end
